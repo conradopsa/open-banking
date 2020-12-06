af34d177b79e87152a09506c1932d6f3d759e74b
darlan.freire <darlan_freirerj@hotmail.com>
2020-12-06 14:26:24 -0300

    autenticado

--- front/src/pages/qrChecked/QrChecked.js
+++ front/src/pages/qrChecked/QrChecked.js
@@ -1,9 +1,14 @@
 import './QrChecked.scss';
+import { AiOutlineCheck } from 'react-icons/ai';
 
 function QrChecked() {
-    return (
-        <p>QR CHECKED FUNCIONA!</p>
-    )
+  return (
+    <div className="format">
+      <p>Autenticado com sucesso!</p>
+      <AiOutlineCheck color="#00D6D6" size="100" />
+      <img src="https://media.tenor.com/images/c7ee1b613a7ddf98ed7c564029bd9c32/tenor.gif"></img>
+    </div>
+  );
 }
 
 export default QrChecked;
